c6f59fadb47a7a650aae3961ec03f8c778c04d90
Vortex2Oblivion <73261680+Vortex2Oblivion@users.noreply.github.com>
2025-10-28 15:14:26 -0400

    play losing anim

--- src/funkin/healthbar.cpp
+++ src/funkin/healthbar.cpp
@@ -7,7 +7,7 @@ funkin::HealthBar::HealthBar(float x, float y, std::string iconLeft, std::string
     add(bar);
     iconP1 = new funkin::HealthIcon(iconRight, 0.0f, 0.0f);
     iconP1->flipX = true;
-        iconP1->origin = raylib::Vector2(0.0f, 0.0f);
+    iconP1->origin = raylib::Vector2(0.0f, 0.0f);
     add(iconP1);
     iconP2 = new funkin::HealthIcon(iconLeft, 0.0f, 0.0f);
     iconP2->origin = raylib::Vector2(300.0f, 0.0f);
@@ -23,6 +23,15 @@ void funkin::HealthBar::update(float delta) {
 
     iconP1->position.x = bar->position.x + bar->getIntersection() - 150.0f + 26.0f * 2.0f + 13.0f;
     iconP1->position.y = bar->position.y - (300.0f - 19.0f) / 2.0f;
+
+    iconP1->playAnimation("default");
+    iconP2->playAnimation("default");
+
+    if (bar->percent >= 80) {
+        iconP2->playAnimation("losing");
+    } else if (bar->percent <= 20) {
+        iconP1->playAnimation("losing");
+    }
 
     float multP1 = Lerp(1.0f, iconP1->scale.x, expf(-delta * 9.0f));
     iconP1->scale = raylib::Vector2(multP1, multP1);
